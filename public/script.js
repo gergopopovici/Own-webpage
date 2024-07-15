1c2f7cacf1b2a54f2a2bd2dfca475d7366c65612
gergopopovici <162745774+gergopopovici@users.noreply.github.com>
2024-07-15 17:01:16 +0300

    feat: Add auto-typing animation and lazy loading for images on about page

--- public/script.js
+++ public/script.js
@@ -5,4 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
     typeSpeed: 150,
     loop: false,
   });
+  // eslint-disable-next-line no-undef
+  new Typed('.auto-type2', {
+    strings: ['I am a third year computer science student at UBB Cluj-Napoca'],
+    typeSpeed: 90,
+    loop: false,
+    onComplete: function () {
+      const imgElement = document.querySelector('.first_part_about img');
+      imgElement.classList.add('show');
+    },
+  });
 });
